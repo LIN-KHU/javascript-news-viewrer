065d04b8308fbaaae12923225a452f906afebbf3
gykwak03 <110815273+gykwak03@users.noreply.github.com>
2024-01-19 16:21:11 +0900

    {header, nav 제작}

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import "./styles";
+import { TAP_NAME } from "./assets/TAB_NAME.js";
 require("dotenv").config();
 
 class App {
   constructor() {
-    const appElement = document.getElementById("app");
-    appElement.innerHTML = "<div>Hello :D</div>";
-    console.log(process.env.API_KEY);
+    this.appElement = document.getElementById("app");
+
+    this.createHeader();
+    this.createNav();
+    this.createSection();
+  }
+
+  createElement(tag, textContent = "") {
+    const element = document.createElement(tag);
+    element.textContent = textContent;
+    return element;
+  }
+
+  createHeader() {
+    const headerElement = this.createElement("header");
+    headerElement.classList.add("header");
+
+    const linkhuText = this.createElement("span", "LINKHU");
+    linkhuText.classList.add("headerFont", "linkhuText");
+    const newsText = this.createElement("span", " - NEWS");
+    newsText.classList.add("headerFont", "newsText");
+
+    this.appElement.appendChild(headerElement);
+    headerElement.appendChild(linkhuText);
+    headerElement.appendChild(newsText);
+  }
+
+  createNav() {
+    const navElement = this.createElement("tab");
+    TAP_NAME.forEach((nav) => {
+      const tabElement = this.createElement("div", nav.ko);
+      tabElement.classList.add("tab");
+      navElement.appendChild(tabElement);
+    });
+    this.appElement.appendChild(navElement);
+  }
+
+  createSection() {
+    const sectionElement1 = this.createElement("section", "section1");
+    this.appElement.appendChild(sectionElement1);
+
+    const sectionElement2 = this.createElement("section", "section2");
+    this.appElement.appendChild(sectionElement2);
   }
 }
 
