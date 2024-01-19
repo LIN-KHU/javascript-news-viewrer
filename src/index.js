d7166d5f2cdc6398c53e473e50aba8d6349c144f
min <kmss0206@gmail.com>
2024-01-19 20:14:46 +0900

    API 집어넣기 완료

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class App {
   constructor() {
     const appElement = document.getElementById("app");
     appElement.innerHTML = "<div>Hello :D</div>";
-    console.log(process.env.API_KEY);
+    console.log(process.env.bf141b39fc91436bbd6c5a758517b26d);
   }
 }
 
