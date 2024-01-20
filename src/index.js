f8831f531c314de5a4fd6b54987dfcf9b9c112db
gykwak03 <110815273+gykwak03@users.noreply.github.com>
2024-01-20 17:55:52 +0900

    {API 중간 저장}

--- src/index.js
+++ src/index.js
@@ -39,6 +39,14 @@ class App {
       navElement.appendChild(tabElement);
     });
     this.appElement.appendChild(navElement);
+
+    const underline1 = this.createElement("div");
+    underline1.classList.add("underline1");
+    this.appElement.appendChild(underline1);
+
+    const underline2 = this.createElement("div");
+    underline2.classList.add("underline2");
+    this.appElement.appendChild(underline2);
   }
 
   createSection() {
@@ -51,3 +59,78 @@ class App {
 }
 
 const app = new App();
+
+const API_KEY = process.env.API_KEY;
+
+var url = `https://newsapi.org/v2/everything?q=Apple&from=2024-01-19&sortBy=popularity&apiKey=${API_KEY}`;
+
+var req = new Request(url);
+fetch(req).then(function (response) {
+  console.log(response.json());
+});
+
+//DOM 구조 생성
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
+const newsList = document.querySelector(".news-lists");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchForm = document.querySelector(".search-form");
+  const searchInput = document.querySelector(".search-input");
+  const newsList = document.querySelector(".news-lists");
+
+  // 이하 생략...
+});
+
+//form 검색 인식하면 이벤트 실행
+searchForm.addEventListener("submit", retrieve);
+
+function retrieve(e) {
+  //form submit 기본 전송기능(action) 막기
+  e.preventDefault();
+
+  let topic = searchInput.value;
+  console.log(topic);
+  const apiKey = "2721d1f0de38415b978ddeed5ff2291a";
+  let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`;
+
+  //검색내용 없을 때는 alert창 띄우기
+  if (topic == "") {
+    alert("먼저 검색어를 입력해주시죨.");
+    return;
+  }
+
+  //news-list의 내용값 비우기(기존 검색된 li 모두 제거)
+  newsList.innerHTML = "";
+
+  //newsApi에서 가져온 정보로 DOM에 출력
+  fetch(url)
+    .then((res) => {
+      //fetch : 요청
+      return res.json();
+    })
+    .then((data) => {
+      //return 되었다면
+      console.log(data);
+      data.articles.forEach((article) => {
+        //html요소들 추가하고 부모요소에 껴주는 구문
+        console.log(article);
+      });
+
+      //관련 뉴스 없을 때
+      let totalResults = data.totalResults;
+      console.log(totalResults);
+      if (totalResults == "0") {
+        console.log("없");
+        let noResullt = document.createElement("strong");
+        noResullt.className = "noResult";
+        noResullt.textContent = "아이쿠 아무런 기사가 없네요ㅠㅠ";
+        newsList.appendChild(noResullt);
+      }
+    })
+    .catch((error) => {
+      console.log("error");
+    });
+
+  console.log(topic);
+}
