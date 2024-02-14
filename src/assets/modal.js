c92f379c6e7fec68498d3ab21f6dc5d0921ba28a
sujin0707 <soojin2650@khu.ac.kr>
2024-02-15 02:47:41 +0900

    기능에 따라 함수 총 2개로 분리

---
+++ src/assets/modal.js
@@ -0,0 +1,113 @@
+export function openModal(article) {
+    const modal = document.getElementById("modal");
+    const closeModal = document.querySelector(".close-modal");
+
+    modal.classList.add("open");
+
+    closeModal.addEventListener("click", () => {
+        modal.classList.remove("open");
+    });
+
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.classList.remove("open");
+      }
+    });
+    
+    const storedOpinion = localStorage.getItem(article);
+    DisplayOpinion(article, storedOpinion);
+  }
+
+  function DisplayOpinion(article, storedOpinion) {
+    const opinionForm = document.getElementById("opinion-form");
+    const opinionTitle = document.getElementById("opinion-title");
+    const opinionContent = document.getElementById("opinion-content");
+    
+    if (storedOpinion) {
+        const parseOpinion = JSON.parse(storedOpinion);
+  
+        const SavedOpinion = document.getElementById("modal-header");
+        SavedOpinion.innerHTML = `
+        <h3>${parseOpinion.title}</h3>
+        <p>${parseOpinion.content}</p>
+        `;
+        
+        opinionTitle.value = parseOpinion.title;
+        opinionContent.value = parseOpinion.content;
+  
+        const saveButton = document.getElementById("save-opinion");
+        let editButton = document.getElementById("edit-opinion");
+        let deleteButton = document.getElementById("delete-opinion");
+  
+        if(saveButton) {
+          saveButton.remove();
+        }
+  
+        if (!editButton) {
+          editButton = document.createElement("button");
+          editButton.textContent = "edit";
+          editButton.id = "edit-opinion";
+    
+          editButton.addEventListener("click", () => {
+            const editedTitle = opinionTitle.value; // trim()을 사용하여 앞뒤 공백을 제거한 값을 가져옵니다.
+            const editedContent = opinionContent.value;
+
+            if (editedTitle !== "" && editedContent !== "") {
+                localStorage.removeItem(article);
+                
+                const opinion = { title:editedTitle, content:editedContent };
+                localStorage.setItem(article, JSON.stringify(opinion));
+                modal.classList.remove("open");
+            } else {
+                alert("제목과 내용을 모두 입력하세요.");
+            }
+          });
+    
+          opinionForm.appendChild(editButton);
+        }
+    
+        if (!deleteButton) {
+          deleteButton = document.createElement("button");
+          deleteButton.textContent = "delete";
+          deleteButton.id = "delete-opinion";
+    
+          deleteButton.addEventListener("click", () => {
+            localStorage.removeItem(article);
+            modal.classList.remove("open");
+          });
+    
+          opinionForm.appendChild(deleteButton);
+        }
+      } else {
+        opinionTitle.value = "";
+        opinionContent.value = "";
+  
+        const saveButton = document.getElementById("save-opinion");
+        const editButton = document.getElementById("edit-opinion");
+        const deleteButton = document.getElementById("delete-opinion");
+  
+        saveButton.textContent = "save";
+        saveButton.addEventListener("click", () => {
+            const savedTitle = opinionTitle.value;
+            const savedContent = opinionContent.value;
+            
+            if (savedTitle !== "" && savedContent !== "") {
+                localStorage.removeItem(article);
+                
+                const opinion = { title: savedTitle, content: savedContent };
+                localStorage.setItem(article, JSON.stringify(opinion));
+                modal.classList.remove("open");
+            } else {
+                alert("제목과 내용을 모두 입력하세요.");
+            }
+        });
+  
+        if (editButton) {
+          editButton.remove();
+        }
+  
+        if (deleteButton) {
+          deleteButton.remove();
+        }
+      }
+  }
